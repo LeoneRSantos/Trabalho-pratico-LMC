6423a3302d9787e5bb06156a7eaf66bb8963b2c5
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-03-26 16:50:56 -0300

    Adição das rotas no main

--- lib/main.dart
+++ lib/main.dart
@@ -21,95 +21,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal.shade900),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Jogo sobre POO'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextStyle texto =
-      const TextStyle(fontSize: 16.0, fontWeight: FontWeight.w700);
-
-  ButtonStyle botoes =
-      const ButtonStyle(elevation: MaterialStatePropertyAll(0.8));
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Center(child: Text(widget.title)),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.all(24.0),
-                child: Center(
-                    child: Text(
-                  "Escolha seu nível de dificuldade.",
-                  style: TextStyle(fontSize: 16.0),
-                )),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ElevatedButton(
-                  style: botoes,
-                  onPressed: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Fácil',
-                      style: texto,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ElevatedButton(
-                  style: botoes,
-                  onPressed: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Médio',
-                      style: texto,
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ElevatedButton(
-                  style: botoes,
-                  onPressed: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Difícil',
-                      style: texto,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      routes: {
+        Rotas.home: (context) => const Home(),
+        Rotas.fundamentoDePoo: (context) => const TelaDeFundamento(),
+        Rotas.telaDoJogo: (context) => const TelaDoJogo(),
+      },
     );
   }
 }
