8ece473a7a363c9685d0cc7faf12c9d3993e3110
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-13 14:31:49 -0300

    Criação da interface para os models do jogo

---
+++ lib/models/interface_jogo.dart
@@ -0,0 +1,25 @@
+abstract class InterfaceJogo {
+  String retornarAtributos(int indice) {
+    return '';
+  }
+
+  int retornarQuantidadeDeAtributos() {
+    return 0;
+  }
+
+  String retornarMetodos(int indice) {
+    return '';
+  }
+
+  int retornarQuantidadeDeMetodos() {
+    return 0;
+  }
+
+  String retornarInstancias(int indice) {
+    return '';
+  }
+
+  int retornarQuantidadeInstancias() {
+    return 0;
+  }
+}
