0010622a4d3926ce5a5f58d59d32c0d43eed652a
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-05-13 14:35:43 -0300

    Adição dos métodos da interface do jogo em Encapsulamento

--- lib/models/encapsulamento.dart
+++ lib/models/encapsulamento.dart
@@ -1,7 +1,6 @@
-import 'package:jogo_sobre_poo/models/tipo.dart';
+import 'package:jogo_sobre_poo/models/interface_jogo.dart';
 
-class Encapsulamento implements Tipo {
-
+class Encapsulamento implements InterfaceJogo {
   final List<String> _listaDeAtributos = [
     'String _nome = \'nome\';',
     'final String nome = \'nome\';',
@@ -21,28 +20,31 @@ class Encapsulamento implements Tipo {
   ];
 
   @override
-  int retornarTipo() => 1;
-
   String retornarAtributos(int indice) {
     return _listaDeAtributos[indice];
   }
 
+  @override
   int retornarQuantidadeDeAtributos() {
     return _listaDeAtributos.length;
   }
 
+  @override
   String retornarMetodos(int indice) {
     return _listaDeMetodos[indice];
   }
 
+  @override
   int retornarQuantidadeDeMetodos() {
     return _listaDeMetodos.length;
   }
 
+  @override
   String retornarInstancias(int indice) {
     return _listaDeInstancias[indice];
   }
 
+  @override
   int retornarQuantidadeInstancias() {
     return _listaDeInstancias.length;
   }
