4490e3efbdd92e12e80b98a157f53efc2a796bfb
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-06-01 19:50:35 -0300

    Adição do provider no main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:jogo_sobre_poo/provider/codigo_provider.dart';
 import 'package:jogo_sobre_poo/routes/rotas.dart';
 import 'package:jogo_sobre_poo/views/home.dart';
 import 'package:jogo_sobre_poo/views/tela_de_poo.dart';
 import 'package:jogo_sobre_poo/views/tela_do_jogo.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,25 +15,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Jogo sobre POO',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          foregroundColor: Colors.grey.shade100,
+    return ChangeNotifierProvider(
+      create: ((context) => CodigoProvider()),
+      child: MaterialApp(
+        title: 'Jogo sobre POO',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            foregroundColor: Colors.grey.shade100,
+          ),
+          fontFamily: 'DMSans',
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color(0xFF05445E),
+          ),
+          useMaterial3: true,
         ),
-        fontFamily: 'DMSans',
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF05445E),
-        ),
-        useMaterial3: true,
+        routes: {
+          Rotas.home: (context) => const Home(),
+          Rotas.fundamentoDePoo: (context) => const TelaDeFundamento(),
+          Rotas.telaDoJogo: (context) => const TelaDoJogo(atributos: [], metodos: [], instancia: [],
+              ),
+        },
       ),
-      routes: {
-        Rotas.home: (context) => const Home(),
-        Rotas.fundamentoDePoo: (context) => const TelaDeFundamento(),
-        Rotas.telaDoJogo: (context) => const TelaDoJogo(atributos: [], metodos: [], instancia: [],
-            ),
-      },
     );
   }
 }
