124ed9385142ad54e1aee6f01befe934e94024b6
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-06-02 15:49:42 -0300

    Adição de métodos com Provider em TelaDoJogo

--- lib/views/tela_do_jogo.dart
+++ lib/views/tela_do_jogo.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:jogo_sobre_poo/provider/codigo_provider.dart';
+import 'package:jogo_sobre_poo/views/tela_do_codigo.dart';
 import 'package:jogo_sobre_poo/widgets/componente_textos.dart';
+import 'package:provider/provider.dart';
 
 class TelaDoJogo extends StatefulWidget {
   const TelaDoJogo({super.key, required this.atributos, required this.metodos, required this.instancia});
@@ -20,6 +23,18 @@ class _TelaDoJogoState extends State<TelaDoJogo> {
       appBar: AppBar(
         backgroundColor: const Color(0xFF189AB4),
         title: const Center(child: Text('Jogo sobre POO')),
+        actions: [IconButton(onPressed: (){ 
+         Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            TelaDoCodigo(),
+                      ),
+                    );
+        }, icon: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(Icons.code, size: 28.0,),
+        ), ),],
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -44,6 +59,7 @@ class _TelaDoJogoState extends State<TelaDoJogo> {
                               padding: const EdgeInsets.only(top: 8.0),
                               child: ElevatedButton(
                                 onPressed: () {
+                                   Provider.of<CodigoProvider>(context, listen: false).adicionarCodigoAtributo(widget.atributos[index]);
                                   debugPrint('${widget.atributos[index]}');
                                 },
                                 child: const Text(
@@ -83,6 +99,7 @@ class _TelaDoJogoState extends State<TelaDoJogo> {
                             Textos(texto: '${widget.metodos[index]}'),
                             ElevatedButton(
                               onPressed: () {
+                                Provider.of<CodigoProvider>(context, listen: false).adicionarCodigoMetodo(widget.metodos[index]);
                                 debugPrint('${widget.metodos[index]}');
                               },
                               child: Padding(
@@ -123,6 +140,7 @@ class _TelaDoJogoState extends State<TelaDoJogo> {
                             padding: const EdgeInsets.only(top: 8.0),
                             child: ElevatedButton(
                               onPressed: () {
+                                Provider.of<CodigoProvider>(context, listen: false).adicionarCodigoInstancia(widget.instancia[index]);
                                 debugPrint('${widget.instancia[index]}');
                               },
                               child: const Text(
