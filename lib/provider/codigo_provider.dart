50b95d2fa3fe834be58eb10b66091ee6c604625b
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2024-06-02 15:46:31 -0300

    Criação de métodos no Provider

--- lib/provider/codigo_provider.dart
+++ lib/provider/codigo_provider.dart
@@ -2,15 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:jogo_sobre_poo/models/codigo.dart';
 
 class CodigoProvider with ChangeNotifier {
-
   Codigo code = Codigo();
-  
 
-  void adicionarCodigo(String codigo){ 
-    code.adicionarCodigo(codigo);
+  void adicionarCodigoAtributo(String codigo) {
+    code.adicionarCodigoAtributo(codigo);
 
     debugPrint(codigo);
 
     notifyListeners();
+  }
+
+  void adicionarCodigoMetodo(String codigo) {
+    code.adicionarCodigoMetodo(codigo);
+
+    debugPrint(codigo);
+
+    notifyListeners();
+  }
+
+  void adicionarCodigoInstancia(String codigo) {
+    code.adicionarCodigoMain(codigo);
+
+    debugPrint(codigo);
+
+    notifyListeners();
+  }
+
+  Text mensagemGanhou(){ 
+    notifyListeners();
+
+    return Text('Ganhou');
   }
 }
